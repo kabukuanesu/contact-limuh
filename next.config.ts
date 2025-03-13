9824d4435b9be147acaadb52f02f4bae4d5b718b
Anesu Kabuku <kabukuanesu@gmail.com>
2025-03-13 17:31:22 +0200

    next output

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // output: "export", // Enable static export
+  output: "export", // Enable static export
   images: {
     unoptimized: true, // Disable image optimization for static export
   },
